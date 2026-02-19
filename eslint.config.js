a32ee8b11ff4de2a1ff58d7b645b6bf76d351b0f
William Thomas <william.arions@gmail.com>
2026-02-19 16:03:51 -0300

    feat:component icon

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+//import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import Text from "./components/text"
+import TrashIcon from "./assets/icons/Trash-Regular.svg?react";
+import Text from "./components/text";
+import Icon from "./components/icon";
+import SpinnerIcon from "./assets/icons/spinner.svg?react"
 
 export default function App() {
- 
-
-  return (
-      <div className="flex flex-col gap-2">
-    <Text variant="body-md" className="text-pink-base">
-      Olá Mundo!
-    </Text>
-    <Text  className="text-green-base">
-      Olá Mundo!
-    </Text>
-    <Text variant="body-sm-bold">
-      Olá Mundo!
-    </Text>
-    </div>
-    
-  )
+	return (
+		<div className="grid gap-3">
+			<div className="flex flex-col gap-2">
+				<Text variant="body-md" className="text-pink-base">
+					Olá Mundo!
+				</Text>
+				<Text className="text-green-base">Olá Mundo!</Text>
+				<Text variant="body-sm-bold">Olá Mundo!</Text>
+			</div>
+			<div className="flex gap-1">
+				<Icon svg={TrashIcon} className="fill-green-base" />
+				<Icon svg={SpinnerIcon} animate/>
+			</div>
+		</div>
+	);
 }
-
